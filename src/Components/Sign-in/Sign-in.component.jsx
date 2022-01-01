07b227a9e03be561c632beab8b72754b45f45a54
Achudan <achuts4@gmail.com>
2022-01-01 15:49:45 -0500

    custom-button design

--- src/Components/Sign-in/Sign-in.component.jsx
+++ src/Components/Sign-in/Sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 import './Sign-in.styles.scss';
 import FormInput from "../Form-Input/form-input.component";
+import CustomButton from "../Custom-Button/custom-button.component";
 
 const Signin = () =>{
     const [name, setName] = useState({email:'',password:''});
@@ -21,7 +22,7 @@ const Signin = () =>{
                 <FormInput name="email" type="email" value={name.email} handleChange={(e)=>handleChange(e)} label="email" required/>
                 <FormInput name="password" type="password" value={name.password} handleChange={(e)=>handleChange(e)} label="password" required/>
 
-                <input type="submit" value="Submit Form"/>
+                <CustomButton type="submit">Sign in</CustomButton>
             </form>
         </div>
     );
