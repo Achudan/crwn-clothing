5f855385d9e57c8bbcde44b5993dc0e398dab420
Achudan <achuts4@gmail.com>
2022-01-04 22:40:49 -0500

    Sign-up component

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from './crown.svg';
 import './header.styles.scss';
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({currentUser}) =>(
+const Header =  ({currentUser}) =>(
     <div className='header'>
         <Link className='logo-container' to="/" >
             <Logo className='logo' />
@@ -14,7 +14,7 @@ const Header = ({currentUser}) =>(
             <Link className="option" to="/contact">CONTACT</Link>
             {
                 currentUser?
-                (<div className="option" onClick={()=>auth.signOut()}>{auth.currentUser.displayName.toUpperCase()}</div>):
+                (<div className="option" onClick={()=>auth.signOut()}>{currentUser.displayName.toUpperCase()}</div>):
                 (<Link className="option" to="/signinsignup">SIGN IN</Link>)
             }
             {console.log('user',currentUser)}
--- src/Pages/Signin-SignupPage/signin-signup.component.jsx
+++ src/Pages/Signin-SignupPage/signin-signup.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './signin-signup.styles.scss';
 import Signin from "../../Components/Sign-in/Sign-in.component";
+import SignUp from "../../Components/Sign-up/Sign-up.component";
 
 const SigninSignup = () =>(
     <div className="sign-in-up">
         <Signin/>
+        <SignUp/>
     </div>
 )
 
